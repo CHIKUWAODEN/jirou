dbce8070063e729f68b722896a053d6f9f40b946
kandayasu <yasuhiro.kanda@gmail.com>
2015-07-19 03:19:21 +0900

    以上、レポっす

--- curl.sh
+++ curl.sh
@@ -35,12 +35,45 @@ curl -s \
 
 
 # Read
-for id in 0 1 2 3 4
+for id in {0..4}
 do
   echo "GET /v1/jirou/${id}"
   curl -s \
     -X GET \
     "localhost:8080/v1/jirou/${id}" | jq "."
+done
+
+
+# Post Report
+POST_DATA=`cat <<EOS
+{
+  "reporter" : "Jhon Smith",
+  "comment" : "lorem ipsum dolor sit amet ... (Free comment)",
+  "rating" : {
+    "noodle" : 5.0,
+    "soup" : 5.0
+  }
+}
+EOS
+`
+for id in {0..4}
+do
+  echo "POST /v1/jirou/${id}/report"
+  echo ${POST_DATA}
+  curl -s \
+    -X POST \
+    -d "${POST_DATA}" \
+    "localhost:8080/v1/jirou/${id}/report" | jq "."
+done
+
+
+# Get Report
+for id in {0..4}
+do
+  echo "GET /v1/jirou/${id}/report"
+  curl -s \
+    -X GET \
+    "localhost:8080/v1/jirou/${id}/report" | jq "."
 done
 
 
--- jirou/server.go
+++ jirou/server.go
@@ -1,6 +1,15 @@
+/*
+Package jirou implements a server and utilities.
+Includes:
+
+	- sever.go  The Server
+	- setup.go  Setup command
+	- help.go   Print help
+*/
 package jirou
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -13,11 +22,13 @@ import (
 	"github.com/syndtr/goleveldb/leveldb/opt"
 )
 
+// Server class
 type Server struct {
 	Router *httprouter.Router
 	Db     *leveldb.DB
 }
 
+// Generate new Server instance
 func NewServer() *Server {
 	var s = new(Server)
 	s.Router = httprouter.New()
@@ -25,6 +36,7 @@ func NewServer() *Server {
 	return s
 }
 
+// API function : "/"
 func (self *Server) Root(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
 	// [todo] - 文字列を []byte にキャストするとメモリコピーが走っちゃうらしい
 	response := []byte(`
@@ -39,6 +51,7 @@ func (self *Server) Root(writer http.ResponseWriter, request *http.Request, _ ht
 	writer.Write(response)
 }
 
+// API function : "/v1"
 func (self *Server) V1Root(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
 	// [todo] - 文字列を []byte にキャストするとメモリコピーが走っちゃうらしい
 	response := []byte(`
@@ -53,6 +66,8 @@ func (self *Server) V1Root(writer http.ResponseWriter, request *http.Request, _ 
 	writer.Write(response)
 }
 
+// API function : "/v1/jirou"
+// Get index of all shops.
 func (self *Server) Search(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
 
 	values := make([]string, 0, 5)
@@ -90,6 +105,8 @@ func (self *Server) Search(writer http.ResponseWriter, request *http.Request, _ 
 // 		writer, "API Under construction", http.StatusNotImplemented)
 // }
 
+// API function : "/v1/jirou"
+// Get a shop data by ID
 func (self *Server) Read(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
 
 	key := []byte(params.ByName("id"))
@@ -122,6 +139,86 @@ func (self *Server) Read(writer http.ResponseWriter, request *http.Request, para
 // 		writer, "API Under construction", http.StatusNotImplemented)
 // }
 
+// API function : GET "/v1/jirou/:id/report"
+func (self *Server) SearchReport(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+	writer.Header().Set("Content-Type", "application/json")
+	http.Error(
+		writer, "API Under construction", http.StatusNotImplemented)
+}
+
+/*
+API function : POST "/v1/jirou/:id/report"
+
+.reporter (string)
+  Reporter name
+
+.comment (string)
+	Free word comment
+
+.rating.noodle (float)
+	麺の評価レートで、0.0 以上 5.0 未満の数値を入力する.
+	0.0 を下回る値は 0.0 に丸められます
+	5.0 を上回る値は 5.0 に丸められます
+
+.rating.soup (float)
+	スープの評価レートで、0.0 以上 5.0 未満の数値を入力する.
+	0.0 を下回る値は 0.0 に丸められます
+	5.0 を上回る値は 5.0 に丸められます
+
+Example:
+
+	{
+		"reporter": "Jhon Smith",
+		"comment": "lorem ipsum dolor sit amet ... (Free comment)",
+		"rating": {
+			"noodle": 5.0,
+			"soup": 5.0
+		}
+	}
+*/
+
+// レポの投稿データ構造に含まれるレーティングの構造
+type Rating struct {
+	Noodle float32 `json:"noodle"`
+	Soup   float32 `json:"soup"`
+}
+
+// レポの投稿データ構造
+type Report struct {
+	Reporter string `json:"reporter"`
+	Comment  string `json:"comment"`
+	Rating   Rating `json:"rating"`
+}
+
+// 以上、レポっす
+func (self *Server) PostReport(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	// Parse POST data
+	var report Report
+	decorder := json.NewDecoder(request.Body)
+	decodeErr := decorder.Decode(&report)
+	if decodeErr != nil {
+		// Error
+		http.Error(writer, "API Under construction", http.StatusNotImplemented)
+		return
+	}
+
+	marshaled, _ := json.Marshal(report)
+	response := []byte(fmt.Sprintf(`
+		{
+			"link" : {
+				"root"   : { "method" : "GET",  "uri" : "/" },
+				"index"  : { "method" : "GET",  "uri" : "/v1" }
+			}, 
+			"content" : %s
+		}
+		`,
+		string(marshaled),
+	))
+	writer.Header().Set("Content-Type", "application/json")
+	writer.Write(response)
+}
+
+// Run up The server
 func (self *Server) Run(serverOption *Option) {
 	// Build Routing
 	router := httprouter.New()
@@ -129,6 +226,8 @@ func (self *Server) Run(serverOption *Option) {
 	router.GET("/v1", self.V1Root)
 	router.GET("/v1/jirou", self.Search)
 	router.GET("/v1/jirou/:id", self.Read)
+	router.GET("/v1/jirou/:id/report", self.SearchReport)
+	router.POST("/v1/jirou/:id/report", self.PostReport)
 	// router.POST("/v1/jirou", self.Create)
 	// router.PUT("/v1/jirou/:id", self.Update)
 	// router.DELETE("/v1/jirou/:id", self.Delete)
